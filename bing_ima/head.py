cc3dbeaf483696f5f70c824f4c16ef0f5a37f8d6
徐瑞泽 <2452234@st.shou.edu.cn>
2024-12-26 01:54:30 +0800

    Update head.py

--- bing_ima/head.py
+++ bing_ima/head.py
@@ -12,7 +12,6 @@ from concurrent.futures import ThreadPoolExecutor
 import gc
 from remove_dup import remove_duplicates
 import argparse
-import warnings
 import asyncio
 import sys
 from rename import rename_photos
